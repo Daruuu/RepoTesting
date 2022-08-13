891b9d7391623662ea8247090992f813192e8053
Daruuu <thedaruny16@gmail.com>
2022-08-14 00:13:17 +0200

    update script v4

--- gitpush.sh
+++ gitpush.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #script to automate my git push in every git repository
+
 git add .
 	echo "Enter the commit message:"
-read COMMITMESSAGE
+	read COMMITMESSAGE
 git commit -m "$COMMITMESSAGE"
-echo  "enter the name of branch (main):"
-read BRANCH
+	echo  "enter the name of branch (main):"
+	read BRANCH
 git push origin $BRANCH
-read
-	echo "REPOSITORY UPDATED SUCCESFUL!!"
+	read
+	echo "------------REPOSITORY UPDATED SUCCESFUL!!------------"
 git log -n 3
